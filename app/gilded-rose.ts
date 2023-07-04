30498a7f2207b6d0737b4cb8597f19fc5f4cde88
Kevin Xie <kevinxiehk@gmail.com>
2023-07-04 12:22:10 +0100

    Added handling of illegal inputs

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -20,26 +20,25 @@ export class GildedRose {
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
       if (this.items[i].name == 'Sulfuras, Hand of Ragnaros') {
+        this.items[i].quality = 80
         continue
       }
 
+      this.items[i].quality = Math.max(0, this.items[i].quality)
+      this.items[i].quality = Math.min(50, this.items[i].quality)
+
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          this.items[i].quality--
-        }
-      } else {
+        this.items[i].quality--
+      }
+      else {
         if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1
+          this.items[i].quality++
           if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
+              this.items[i].quality = this.items[i].quality + 1
             }
             if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
+              this.items[i].quality = this.items[i].quality + 1
             }
           }
         }
@@ -58,6 +57,9 @@ export class GildedRose {
             this.items[i].quality--
         }
       }
+
+      this.items[i].quality = Math.max(0, this.items[i].quality)
+      this.items[i].quality = Math.min(50, this.items[i].quality)
     }
 
     return this.items;
