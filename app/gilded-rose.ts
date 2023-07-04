fe317067c034bbeca61f2f3ea1093f74bf393abc
JackStrange <jack.d.strange.business@gmail.com>
2023-07-04 14:28:02 +0100

    Remove one-line ifs

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -29,25 +29,43 @@ export class GildedRose {
   }
 
   getItemQualityChange(item:Item){
-    if(item.name === "Aged Brie") return this.getAgedBrieQualityChange(item);
-    if(item.name.includes("Backstage passes",0)) return this.getPassesQualityChange(item);
+    if(item.name === "Aged Brie") {
+      return this.getAgedBrieQualityChange(item);
+    }
+    if(item.name.includes("Backstage passes",0)) {
+      return this.getPassesQualityChange(item);
+    }
 
-    if(item.sellIn < 0 && item.quality > 1) return -2;
-    if(item.quality > 0) return -1;
+    if(item.sellIn < 0 && item.quality > 1) {
+      return -2;
+    }
+    if(item.quality > 0) {
+      return -1;
+    }
     return 0;
   }
 
   getAgedBrieQualityChange(item:Item){
-    if(item.sellIn < 0 && item.quality < 49) return 2;
-    if(item.quality < 50) return 1;
+    if(item.sellIn < 0 && item.quality < 49) {
+      return 2;
+    }
+    if(item.quality < 50) {
+      return 1;
+    }
     return 0;
   }
 
   getPassesQualityChange(item:Item){
     if(item.sellIn < 0) return -item.quality;
-    if(item.sellIn <= 5 && item.quality < 48) return 3;
-    if(item.sellIn <= 10 && item.quality < 49) return 2;
-    if(item.quality < 50) return 1;
+    if(item.sellIn <= 5 && item.quality < 48) {
+      return 3;
+    }
+    if(item.sellIn <= 10 && item.quality < 49) {
+      return 2;
+    }
+    if(item.quality < 50) {
+      return 1;
+    }
     return 0;
   }
 }
