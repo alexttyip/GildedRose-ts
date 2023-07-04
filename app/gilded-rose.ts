0f8b35c62b38d57f85e134c044c6a6a084a67f3a
ChhDhi <dchhavi762@gmail.com>
2023-07-04 15:05:12 +0100

    -- wrote tests -- refactored code by creating functions

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -17,7 +17,8 @@ export class GildedRose {
     this.items = items;
   }
 
-  updateQuality() {
+
+  /*updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality > 0) {
@@ -65,5 +66,83 @@ export class GildedRose {
     }
 
     return this.items;
+  }*/
+
+  updateQuality() {
+    for (let i = 0; i < this.items.length; i++) {
+
+      if(this.items[i].name === 'Aged Brie') {
+        this.updateBrie(this.items[i]);
+      } else if(this.items[i].name === 'Conjured Mana Cake') {
+        this.updateConjured(this.items[i]);
+      } else if (this.items[i].name === 'Sulfuras, Hand of Ragnaros') {
+        //do nothing
+      } else if (this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert'){
+        this.updateBackStagePass(this.items[i]);
+      } else {
+        this.updateItem(this.items[i]);
+      }
+    }
+    return this.items;
   }
+
+  updateBrie( item) {
+    if(item.sellIn <= 0) {
+      this.increaseByMax(item, 2)
+    } else {
+      this.increaseByMax(item, 1)
+    }
+
+    item.sellIn--;
+  }
+
+  updateBackStagePass(item) {
+    if(item.sellIn <= 5 && item.sellIn > 0) {
+      this.increaseByMax(item, 3);
+    } else if (item.sellIn <= 10 && item.sellIn > 0) {
+      this.increaseByMax(item, 2);
+    } else if (item.sellIn > 10) {
+      this.increaseByMax(item, 1);
+    } else {
+      item.quality = 0;
+    }
+
+    item.sellIn--;
+  }
+
+
+
+  updateItem(item) {
+    if(item.sellIn <= 0) {
+      this.decreaseByMax(item, 2);
+    } else {
+      this.decreaseByMax(item, 1);
+    }
+    item.sellIn--;
+  }
+
+  updateConjured(item) {
+    if(item.sellIn <= 0) {
+      this.decreaseByMax(item, 4);
+    } else {
+      this.decreaseByMax(item, 2);
+    }
+    item.sellIn--;
+  }
+
+  decreaseByMax(item, max) {
+    while(max > 0 && item.quality > 0) {
+      item.quality--;
+      max--;
+    }
+  }
+
+  increaseByMax(item, max) {
+    while(max > 0 && item.quality < 50) {
+      item.quality++;
+      max--;
+    }
+  }
+
+
 }
--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -4,6 +4,104 @@ describe('Gilded Rose', () => {
   it('should foo', () => {
     const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
+    expect(items[0].name).toBe('foo');
   });
 });
+
+describe('Quality degrades twice as fast after sell by', () => {
+  it('should decrease 2x', () => {
+    const gildedRose = new GildedRose([new Item('foo', 0, 4), new Item('bar', 1, 4)]);
+    const items = gildedRose.updateQuality();
+    const fooDecrease = 4 - items[0].quality;
+    const barDecrease = 4 - items[1].quality;
+    expect(fooDecrease).toBe(2*barDecrease);
+  });
+});
+
+describe('Quality should not be negative', () => {
+  it('should not decrease below 0', () => {
+    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBeGreaterThanOrEqual(0);
+  });
+});
+
+describe('Aged Brie not past sell by', () => {
+  it('should increase', () => {
+    const gildedRose = new GildedRose([new Item('Aged Brie', 2, 4)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBeGreaterThan(4);
+  });
+});
+
+describe('Aged Brie past sell by', () => {
+  it('rate of increase should double', () => {
+    const gildedRose = new GildedRose([new Item('Aged Brie', 0, 4), new Item("Aged Brie", 5, 4)]);
+    const items = gildedRose.updateQuality();
+    const expiredIncrease = items[0].quality - 4;
+    const inDateIncrease = items[1].quality - 4;
+    expect(expiredIncrease).toBe(2 * inDateIncrease);
+  });
+});
+
+describe('Quality cap', () => {
+  it('should not increase beyond 50', () => {
+    const gildedRose = new GildedRose([new Item('Aged Brie', 50, 50)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBeLessThanOrEqual(50);
+  });
+});
+
+
+describe('Quality for sulfuras', () => {
+  it('should not change', () => {
+    const gildedRose = new GildedRose([new Item('Sulfuras, Hand of Ragnaros', 0, 80)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(80);
+  });
+});
+
+describe('Sell by for sulfuras', () => {
+  it('should not change', () => {
+    const gildedRose = new GildedRose([new Item('Sulfuras, Hand of Ragnaros', 15, 80)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(15);
+  });
+});
+
+describe('Quality for backstage passes -- 10 days or less', () => {
+  it('should increase by 2', () => {
+    const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 10, 20)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(22);
+  });
+});
+
+describe('Quality for backstage passes -- 5 days or less', () => {
+  it('should increase by 3', () => {
+    const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 5, 20)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(23);
+  });
+});
+
+describe('Quality for backstage passes -- 0 days', () => {
+  it('should decrease to 0', () => {
+    const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 0, 20)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(0);
+  });
+});
+
+
+
+describe('Conjured item', () => {
+  it('should decrease twice as fast', () => {
+    const gildedRose = new GildedRose([new Item('Conjured Mana Cake', 2, 4), new Item('bar', 1, 4)]);
+    const items = gildedRose.updateQuality();
+    const conjuredDecrease = 4 - items[0].quality;
+    const barDecrease = 4 - items[1].quality;
+    expect(conjuredDecrease).toBe(2*barDecrease);
+  });
+});
+
