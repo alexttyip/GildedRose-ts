2dcbf75bdc886b972983b68ddcdb74c216dff289
Kevin Xie <kevinxiehk@gmail.com>
2023-07-04 13:55:51 +0100

    added and used updateOneQuality function

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -17,62 +17,67 @@ export class GildedRose {
     this.items = items;
   }
 
-  decrease(v) : number {
+  static decrease(v) : number {
     // cannot decrease to lower than 0
     return Math.max(v - 1, 0)
   }
 
-  increase(v) : number {
+  static increase(v) : number {
     // cannot decrease to higher than 50
     return Math.min(v + 1, 50)
   }
 
-  updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name == 'Sulfuras, Hand of Ragnaros') {
-        // Special handling for Sulfuras
-        continue
-      }
+  static updateOneQuality(item : Item) : Item {
+    // Updates one item
 
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        // Decrease quality
-        this.items[i].quality = this.decrease(this.items[i].quality)
-      }
-      else {
-        // Increase quality
-        this.items[i].quality = this.increase(this.items[i].quality)
+    if (item.name == 'Sulfuras, Hand of Ragnaros') {
+      // Special handling for Sulfuras
+      return item
+    }
 
-        // Concert ticket, more increases
-        if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (this.items[i].sellIn < 11) {
-            this.items[i].quality = this.increase(this.items[i].quality)
-          }
-          if (this.items[i].sellIn < 6) {
-            this.items[i].quality = this.increase(this.items[i].quality)
-          }
+    if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+      // Decrease quality
+      item.quality = GildedRose.decrease(item.quality)
+    }
+    else {
+      // Increase quality
+      item.quality = GildedRose.increase(item.quality)
+
+      // Concert ticket, more increases
+      if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
+        if (item.sellIn < 11) {
+          item.quality = GildedRose.increase(item.quality)
         }
-      }
-
-      // Decrease sell by date
-      this.items[i].sellIn = this.items[i].sellIn - 1;
-
-      if (this.items[i].sellIn < 0) {
-        // Overdue handling
-        if (this.items[i].name == 'Aged Brie') {
-          // Aged Brie increases quality
-          this.items[i].quality = this.increase(this.items[i].quality)
-        }
-        else if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-          // Concert ticket has quality decreased to 0
-          this.items[i].quality = 0
-        }
-        else {
-          // Decrease quality
-          this.items[i].quality = this.decrease(this.items[i].quality)
+        if (item.sellIn < 6) {
+          item.quality = GildedRose.increase(item.quality)
         }
       }
     }
 
+    // Decrease sell by date
+    item.sellIn--
+
+    if (item.sellIn < 0) {
+      // Overdue handling
+      if (item.name == 'Aged Brie') {
+        // Aged Brie increases quality
+        item.quality = GildedRose.increase(item.quality)
+      }
+      else if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
+        // Concert ticket has quality decreased to 0
+        item.quality = 0
+      }
+      else {
+        // Decrease quality
+        item.quality = GildedRose.decrease(item.quality)
+      }
+    }
+
+    return item
+  }
+
+  updateQuality() {
+    this.items.map(GildedRose.updateOneQuality)
     return this.items;
   }
 }
