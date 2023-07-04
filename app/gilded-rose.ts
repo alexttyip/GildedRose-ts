81ff9bbf67fd37cb22e321aa817092671bfd387a
Connor Whitworth <Connor.Whitworth@softwire.com>
2023-07-04 14:20:19 +0100

    Simplified, replaced nested statements

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -20,59 +20,48 @@ export class GildedRose {
   }
 
   // Can Change this
+
+  calculateQualityChange() {
+
+  }
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name.toLowerCase().indexOf('backstage passes') === -1) {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name.toLowerCase().indexOf('sulfuras') === -1) {
-            this.items[i].quality -= 1
+      if (this.items[i].name.toLowerCase().indexOf('sulfuras') === -1) {
+        this.items[i].sellIn -= 1
+      }
+      let qualityChange = 0;
+      if (this.items[i].name == 'Aged Brie' || this.items[i].name.toLowerCase().includes('backstage passes')) {
+        qualityChange = 1
+        if (this.items[i].name.toLowerCase().includes('backstage passes')) {
+          if (this.items[i].sellIn <= 10) {
+            qualityChange += 1;
           }
-          if (this.items[i].name.toLowerCase().includes('conjured')) {
-            this.items[i].quality -= 1
+          if (this.items[i].sellIn <= 5) {
+            qualityChange += 1;
           }
+          if (this.items[i].sellIn < 0) {
+            qualityChange = -1 * this.items[i].quality;
+          }
+        }
+        if (this.items[i].quality + qualityChange > 50) {
+          qualityChange = 50 - this.items[i].quality;
+        }
+      } else if (this.items[i].name.toLowerCase().indexOf('sulfuras') === -1) {
+        qualityChange = -1;
+        if (this.items[i].sellIn < 0) {
+          qualityChange *= 2;
         }
       } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality += 1
-          if (this.items[i].name.toLowerCase().includes('backstage passes')) {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality += 1
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality += 1
-              }
-            }
-          }
-        }
+        qualityChange = 0;
       }
-      if (this.items[i].name.toLowerCase().indexOf('sulfuras') === -1) {
-        this.items[i].sellIn -= 1;
+      if (this.items[i].name.toLowerCase().includes('conjured')) {
+        qualityChange *= 2;
       }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name.toLowerCase().indexOf('backstage passes') === -1) {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name.toLowerCase().indexOf('sulfuras') === -1) {
-                this.items[i].quality -= 1
-              }
-              if (this.items[i].name.toLowerCase().includes('conjured')) {
-                this.items[i].quality -= 1
-              }
-            }
-          } else {
-            this.items[i].quality = 0
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality += 1
-          }
-        }
+      this.items[i].quality += qualityChange;
+      if (this.items[i].quality < 0) {
+        this.items[i].quality = 0;
       }
     }
-
     return this.items;
   }
 }
