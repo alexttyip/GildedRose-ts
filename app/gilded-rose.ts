1386d7eb26ae9634e66140dc88458692074c696f
Connor Whitworth <Connor.Whitworth@softwire.com>
2023-07-04 12:07:26 +0100

    Conjured functionality added

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -27,6 +27,9 @@ export class GildedRose {
           if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
             this.items[i].quality = this.items[i].quality - 1
           }
+          if (this.items[i].name == 'Conjured Mana Cake') {
+            this.items[i].quality = this.items[i].quality - 1
+          }
         }
       } else {
         if (this.items[i].quality < 50) {
@@ -53,6 +56,9 @@ export class GildedRose {
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].quality > 0) {
               if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+                this.items[i].quality = this.items[i].quality - 1
+              }
+              if (this.items[i].name == 'Conjured Mana Cake') {
                 this.items[i].quality = this.items[i].quality - 1
               }
             }
