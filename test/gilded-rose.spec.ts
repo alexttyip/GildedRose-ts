ec4ae8bbbb6f85d94c90035e5b6046243c46d8a6
JackStrange <jack.d.strange.business@gmail.com>
2023-07-04 12:12:23 +0100

    Implement standard rule tests

--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -1,9 +1,87 @@
 import { Item, GildedRose } from '@/gilded-rose';
 
 describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+  it('Given an item with a SellIn value >0, when a day passes, the SellIn value should drop by 1', () => {
+    // Given
+    const gildedRose = new GildedRose([new Item('test', 10, 0)]);
+
+    // When
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
+
+    // Then
+    expect(items[0].sellIn).toBe(9);
+  });
+
+  it('Given an item with a SellIn time of 0, when a day passes, its quality should drop by double the amount', () => {
+    // Given
+    const gildedRose = new GildedRose([new Item('test', 1, 20)]);
+    //   For test consistency, access the quality update value by an initial update
+    let items = gildedRose.updateQuality();
+    const qualityDropRate = 20 - items[0].quality;
+
+    // When
+    items = gildedRose.updateQuality();
+
+    // Then
+    expect(items[0].quality).toBe(20 - qualityDropRate*3);
+  });
+
+  it('Given an item with a SellIn time of <0, when a day passes, its quality should drop by double the amount', () => {
+    // Given
+    const gildedRose = new GildedRose([new Item('test', 1, 20)]);
+    //   For test consistency, access the quality update value by an initial update
+    let items = gildedRose.updateQuality();
+    const qualityDropRate = 20 - items[0].quality;
+    items = gildedRose.updateQuality();
+
+    // When
+    items = gildedRose.updateQuality();
+
+    // Then
+    expect(items[0].quality).toBe(20 - qualityDropRate*5);
+  });
+
+  it('Given an item with a quality value of 0, when a day passes, its quality should remain at 0', () => {
+    // Given
+    const gildedRose = new GildedRose([new Item('test', 5, 0)]);
+
+    // When
+    const items = gildedRose.updateQuality();
+
+    // Then
+    expect(items[0].quality).toBe(0);
+  });
+
+  it('Given an item with a quality value of 0 and sellIn value of 0, when a day passes, its quality should remain at 0', () => {
+    // Given
+    const gildedRose = new GildedRose([new Item('test', 0, 0)]);
+
+    // When
+    const items = gildedRose.updateQuality();
+
+    // Then
+    expect(items[0].quality).toBe(0);
+  });
+
+  it('Given an item with a quality value <0, when a day passes, its quality should be set to 0', () => {
+    // Given
+    const gildedRose = new GildedRose([new Item('test', 5, -10)]);
+
+    // When
+    const items = gildedRose.updateQuality();
+
+    // Then
+    expect(items[0].quality).toBe(0);
+  });
+
+  it('Given an item with a quality value >50, when a day passes, its quality should be set to 50', () => {
+    // Given
+    const gildedRose = new GildedRose([new Item('test', 5, 80)]);
+
+    // When
+    const items = gildedRose.updateQuality();
+
+    // Then
+    expect(items[0].quality).toBe(50);
   });
 });
