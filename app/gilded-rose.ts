ba58afeb6ca900a2c5a669e4534595ec21dae224
Connor Whitworth <Connor.Whitworth@softwire.com>
2023-07-04 12:23:16 +0100

    Generalised product names as criteria

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -22,19 +22,19 @@ export class GildedRose {
   // Can Change this
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+      if (this.items[i].name != 'Aged Brie' && this.items[i].name.toLowerCase().indexOf('backstage passes') === -1) {
         if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+          if (this.items[i].name.toLowerCase().indexOf('sulfuras') === -1) {
             this.items[i].quality -= 1
           }
-          if (this.items[i].name == 'Conjured Mana Cake') {
+          if (this.items[i].name.toLowerCase().includes('conjured')) {
             this.items[i].quality -= 1
           }
         }
       } else {
         if (this.items[i].quality < 50) {
           this.items[i].quality += 1
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+          if (this.items[i].name.toLowerCase().includes('backstage passes')) {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
                 this.items[i].quality += 1
@@ -48,17 +48,17 @@ export class GildedRose {
           }
         }
       }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+      if (this.items[i].name.toLowerCase().indexOf('sulfuras') === -1) {
         this.items[i].sellIn -= 1;
       }
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+          if (this.items[i].name.toLowerCase().indexOf('backstage passes') === -1) {
             if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+              if (this.items[i].name.toLowerCase().indexOf('sulfuras') === -1) {
                 this.items[i].quality -= 1
               }
-              if (this.items[i].name == 'Conjured Mana Cake') {
+              if (this.items[i].name.toLowerCase().includes('conjured')) {
                 this.items[i].quality -= 1
               }
             }
